57f3092ee669eac8fae2e760c13ea8f45370654a
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-22 13:07:07 +0800

    feat: add badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Philippines } from "@/components/philippines";
+import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { Toaster } from "sonner";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <Philippines />
+      <Badge variant="outline" className="fixed top-2 right-2">For Demo Purposes Only</Badge>
       <div className="fixed top-10 right-10">
         <ModeToggle />
       </div>
